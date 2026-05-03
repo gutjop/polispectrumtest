3ccb4986e9984b8325ffef676fcdd27d2dc52353
gutjop <92240210+gutjop@users.noreply.github.com>
2026-05-03 06:50:25 -0700

    fixed typo

--- questions.js
+++ questions.js
@@ -80,7 +80,7 @@
     {
         id: 14, //32
         category: "social",
-        text: "It is an acceptable to limitation of free speech to prohibit hate speech.",
+        text: "It is an acceptable limitation of free speech to prohibit hate speech.",
         options: ["Strongly Disagree", "Disagree", "Agree", "Strongly Agree", "Neutral"]
     },
     {
